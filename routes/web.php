a008a5eac2b58db7755d514a19f7a2e01d2e3db9
nazwaalifah27-svg <nazwaalifah27@gmail.com>
2025-10-15 13:31:16 +0700

    PUSH pertama saya untuk project Laravel menggunakan Bootstrap 5

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function ()  {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('/profile', function () {
+    $nama = 'Najwa Alifah Zulfikar Damanik';
+    //return view('profile' , compact ('nama'));
+    return view('profile')->with('nama', $nama);
+});
