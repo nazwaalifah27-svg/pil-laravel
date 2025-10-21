0ad61b53d4af26744ce736cbdb8ab8060980d836
nazwaalifah27-svg <nazwaalifah27@gmail.com>
2025-10-21 21:39:16 +0700

    Belajar For

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,8 @@ Route::get('/profile', function () {
 });
 
 Route::get('array', function () {
-    
-    $nilai_awal = 1;
 
-    while ($nilai_awal <= 100) {
-        echo 'Hello Laravel ' . $nilai_awal . 'x<br>';
-        $nilai_awal++;
+    for ($i = 1; $i <= 5; $i++) {
+        echo 'Hello World ' . $i . 'x<br>';
     }
-
 });
