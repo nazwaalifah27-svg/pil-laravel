d4d60f22552cb4e675bd16b69f7511cc4a322867
nazwaalifah27-svg <nazwaalifah27@gmail.com>
2025-10-21 23:10:43 +0700

    Belajar membuat FORM didalam halaman mahasiswa

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -1,70 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mahasiswa</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Mahasiswa</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+        crossorigin="anonymous">
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/">Laravel</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/mahasiswa">Mahasiswa</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/profile">Profile</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Laravel</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
+              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+              aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/mahasiswa">Mahasiswa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/profile">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About</a>
+          </li>
+        </ul>
+
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-4" style="margin-left: 30px">
+    <h1>Ini adalah halaman Mahasiswa</h1>
+
+    <div class="row">
+      <!-- Kolom kiri: Tabel Mahasiswa -->
+      <div class="col-sm-6">
+        <h4>Tabel Mahasiswa</h4>
+        <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
+          <thead>
+            <tr>
+              <th>NIM</th>
+              <th>Nama Mahasiswa</th>
+              <th>Jenis Kelamin</th>
+              <th colspan="2">TTL</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php 
+              $npm = ['01', '02', '03', '04'];
+              $nama = ['Najwa', 'Alifah', 'Zulfikar', 'Damanik'];
+            ?>
+            @for ($i = 0; $i < count($npm); $i++)
+              <tr>
+                <td>{{ $npm[$i] }}</td>
+                <td>{{ $nama[$i] }}</td>
+                <td>Wanita</td>
+                <td>Kota Medan</td>
+              </tr>
+            @endfor
+          </tbody>
+        </table>
+      </div>
+
+      <!-- Kolom kanan: Form Mahasiswa -->
+      <div class="col-sm-6">
+        <h4>Form Mahasiswa</h4>
+        <form action="" method="GET">
+          <div class="row">
+            <div class="col-sm-6">
+              <label for="">NPM</label>
+              <input type="number" name="npm" class="form-control" placeholder="Input NPM">
+            </div>
+
+            <div class="col-sm-6">
+              <label for="">Nama Mahasiswa</label>
+              <input type="text" name="nama_mahasiswa" class="form-control" placeholder="Input Nama Mahasiswa">
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col-sm-6">
+              <label for="">Tanggal Lahir</label>
+              <input type="date" name="tgl_lahir" class="form-control">
+            </div>
+
+            <div class="col-sm-6">
+              <label for="">Prodi</label>
+              <select name="prodi" class="form-control">
+                <option value="">Sistem Informasi</option>
+                <option value="">Teknik Informatika</option>
+                <option value="">Sains Data</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="row mt-3">
+            <div class="col-sm-12">
+              <button class="btn btn-primary" style="width: 100%">Simpan</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-</nav>
 
- <div class="container" style="margin-left: 30px">
-  <h1>Ini adalah halaman Mahasiswa</h1>
-
-  <table class="table table-danger table-sm table-hover table-striped table-bordered text-center">
-  <thead>
-    <tr>
-      <th>NIM</th>
-      <th>Nama Mahasiswa</th>
-      <th>Jenis Kelamin</th>
-      <th colspan="2">TTL</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php 
-      $npm = ['01', '02', '03', '04'];
-      $nama = ['Najwa', 'Alifah', 'Zulfikar', 'Damanik'];
-    ?>
-
-    @for ($i = 0; $i < count($npm); $i++)
-      <tr>
-        <td>{{ $npm[$i] }}</td>
-        <td>{{ $nama[$i] }}</td>
-        <td>Wanita</td>
-        <td>Kota Medan</td>
-      </tr>
-    @endfor
-  </tbody>
-</table>
-
- </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" 
+          crossorigin="anonymous"></script>
 </body>
 </html>
